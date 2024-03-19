a5153ef95f1ec3a7f84854bb75ea7a0aee3f3eb1
wortie <aircwo@gmail.com>
2024-03-19 20:23:30 +0000

    chore: add test env setup for env variable

--- jest.config.ts
+++ jest.config.ts
@@ -5,10 +5,11 @@ export default {
   preset: 'ts-jest',
   testMatch: ['**/*.test.ts'],
   moduleDirectories: ['node_modules'],
+  setupFiles: ['<rootDir>/tests/setup.ts'], // sets up fake environtment variables for test
   coverageThreshold: {
     global: {
-      branches: 50,
-      functions: 55,
+      branches: 60,
+      functions: 60,
       lines: 70,
       statements: 70,
     },
